d2f77226fb17cf0e6f24b2e2f2a16e1e8bac6171
Jan Horava <czechinthemail@gmail.com>
2013-06-19 19:01:18 -0700

    improved formatting

--- js/pixeleditor.js
+++ js/pixeleditor.js
@@ -1,9 +1,9 @@
 function get(i){
-    return document.getElementById(i)
+    return document.getElementById(i);
 }
 
 function make_png(){
-    window.open(get('canvas').toDataURL('image/png'))
+    window.open(get('canvas').toDataURL('image/png'));
 }
 
 function reset(){
@@ -12,39 +12,39 @@ function reset(){
 
         i = 624;
         do{
-            get(i).style.backgroundColor = '#000'
+            get(i).style.backgroundColor = '#000';
         }while(i--);
 
-        /*set view to edit mode*/
+        /* set view to edit mode */
         view = 1;
-        switch_view()
+        switch_view();
     }
 }
 
 function set_pixeldiv_color(i){
     warn_onbeforeunload = 1;
-    /*verify color values*/
+    /* verify color values */
     if(isNaN(get('red').value) || get('red').value < 0 || get('red').value > 255){
-        get('red').value = 255
+        get('red').value = 255;
     }
     if(isNaN(get('green').value) || get('green').value < 0 || get('green').value > 255){
-        get('green').value = 255
+        get('green').value = 255;
     }
     if(isNaN(get('blue').value) || get('blue').value < 0 || get('blue').value > 255){
-        get('blue').value = 255
+        get('blue').value = 255;
     }
 
-    /*set pixel div background*/
+    /* set pixel div background */
     get(i).style.background = 'rgb(' + parseInt(get('red').value) + ','
         + parseInt(get('green').value) + ','
-        + parseInt(get('blue').value) + ')'
+        + parseInt(get('blue').value) + ')';
 }
 
 function switch_view(){
     view = !view;
 
-    if(view){/*preview mode*/
-        /*paint canvas pixels based on colors of divs*/
+    if(view){/* preview mode */
+        /* paint canvas pixels based on colors of divs */
         get('canvas').height = 25;
         get('canvas').width = 25;
         j = 25;
@@ -60,14 +60,14 @@ function switch_view(){
             );
             x.fillStyle = '#000';
             if(i % 25 === 0){
-                j -= 1
+                j -= 1;
             }
-        }while(i--)
+        }while(i--);
     }
 
     get('switch-button').value = view ? 'Edit' : 'Preview';
     get('edit-div').style.display = view ? 'none' : 'inline';
-    get('preview-div').style.display = view ? 'inline' : 'none'
+    get('preview-div').style.display = view ? 'inline' : 'none';
 }
 
 var i = 624;
@@ -76,11 +76,11 @@ var view = 0;
 var warn_onbeforeunload = 0;
 var x = 0;
 
-/*create pixel divs*/
+/* create pixel divs */
 do{
     j += '<div class=pixel id=' + i + ' onclick=set_pixeldiv_color(' + i + ')></div>';
     if(i % 25 === 0){
-        j += '<br>'
+        j += '<br>';
     }
 }while(i--);
 
@@ -88,7 +88,7 @@ get('edit-div').innerHTML = j;
 j = 0;
 
 window.onbeforeunload = function(){
-    if(warn_onbeforeunload){/*if any pixels have been changed*/
-        return'Save feature not yet implemented.'
+    if(warn_onbeforeunload){/* if any pixels have been changed */
+        return'Save feature not yet implemented.';
     }
 }

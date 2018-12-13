6806e9c97cdb1fa7e2e6bcb63094fbadef5b4516
Jan Hořava <czechinthemail@gmail.com>
2018-12-12 16:08:38 -0800

    added file-to-uri inputs

--- js/main.js
+++ js/main.js
@@ -3,6 +3,21 @@
 function repo_init(){
     core_repo_init({
       'events': {
+        'file-to-uri': {
+          'onclick': function(){
+              let files = document.getElementById('file').files;
+              if(files.length === 0){
+                  return;
+              }
+
+              core_file({
+                'file': files[0],
+                'todo': function(event){
+                    document.getElementById('uri').value = event.target.result;
+                },
+              });
+          },
+        },
         'fill': {
           'onclick': fill,
         },
@@ -35,6 +50,7 @@ function repo_init(){
         'view': false,
       },
       'info': '<textarea id=uri></textarea><br><canvas id=canvas></canvas> <span id=uri-length></span><hr>'
+        + '<input id=file type=file><input id=file-to-uri type=button value="Convert File to URI"><br>'
         + '<input id=grid-remake type=button value="Remake Grid"><input id=grid-toggle type=button value="Toggle Borders"><input id=uri-to-grid type=button value="Set Grid to URI"><br>'
         + '<input id=color type=color value=#ffffff><input id=fill type=button value=Fill>'
         + ' <span id=x></span> <span id=y></span> <input id=color-hover readonly>',

3bb5b46294545c9b4262a5ac22cc2234c08d135e
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 14:13:51 -0700

    combined js into repo.js

--- repo.js
+++ repo.js
@@ -52,6 +52,84 @@ function hover_pixel(pixel){
     }
 }
 
+function repo_init(){
+    core_repo_init({
+      'events': {
+        'file-to-uri': {
+          'onclick': function(){
+              const files = document.getElementById('file').files;
+              if(files.length === 0){
+                  return;
+              }
+
+              core_file({
+                'file': files[0],
+                'todo': function(event){
+                    document.getElementById('uri').value = event.target.result;
+                },
+              });
+          },
+        },
+        'fill': {
+          'onclick': fill,
+        },
+        'grid-remake': {
+          'onclick': function(){
+              if(!globalThis.confirm('Remake grid?')){
+                  return;
+              }
+
+              core_storage_save();
+              setup_dimensions();
+          },
+        },
+        'grid-toggle': {
+          'onclick': grid_toggle,
+        },
+        'uri-to-grid': {
+          'onclick': function(){
+              if(!globalThis.confirm('Set grid to URI?')){
+                  return;
+              }
+
+              core_storage_save();
+              uri_to_grid();
+          },
+        },
+      },
+      'globals': {
+        'pixelcount': 0,
+        'view': false,
+      },
+      'info': '<textarea id=uri></textarea><br><canvas id=canvas style="border:solid 10px #000"></canvas> <span id=uri-length></span><hr>'
+        + '<input id=file type=file><input id=file-to-uri type=button value="Convert File to URI"><br>'
+        + '<input id=grid-remake type=button value="Remake Grid"><input id=grid-toggle type=button value="Toggle Borders"><input id=uri-to-grid type=button value="Set Grid to URI"><br>'
+        + '<input id=color type=color value=#ffffff><input id=fill type=button value=Fill>'
+        + ' <span id=x></span> <span id=y></span> <input class=mini id=color-hover readonly type=text>',
+      'menu': true,
+      'menu-block-events': false,
+      'storage': {
+        'height': 32,
+        'mode': 0,
+        'quality': 1,
+        'size': 25,
+        'type': 'image/png',
+        'uri': '',
+        'width': 32,
+      },
+      'storage-menu': '<table><tr><td><input class=mini id=height min=1 step=any type=number><td>Height'
+        + '<tr><td><select id=mode><option value=1>Color Picking</option><option value=0>Set Pixel Color</option><option value=2>Transparency</option></select><td>Mode'
+        + '<tr><td><input class=mini id=quality max=1 min=0 step=any type=number><td>Quality'
+        + '<tr><td><input class=mini id=size min=1 step=any type=number><td>px Size'
+        + '<tr><td><input id=type type=text><td>Type'
+        + '<tr><td><input class=mini id=width min=1 step=any type=number><td>Width</table>',
+      'title': 'PixelEditor.htm',
+    });
+
+    setup_dimensions();
+    document.getElementById('edit').style.userSelect = 'none';
+}
+
 function rgb_to_hex(rgb){
     rgb = rgb.slice(4);
 

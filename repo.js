d5f92d616ab9325120bd3d6ba476bb4c91b5293e
Jan Hořava <czechinthemail@gmail.com>
2025-06-15 11:38:15 -0700

    simplified property usage

--- repo.js
+++ repo.js
@@ -1,9 +1,9 @@
 'use strict';
 
 function fill(){
-    const type = core_storage_data['mode'] === 2
+    const type = core_storage_data.mode === 2
       ? 'transparent'
-      : core_elements['color'].value;
+      : core_elements.color.value;
 
     if(!globalThis.confirm('Set every pixel to ' + type + '?')){
         return;
@@ -33,16 +33,16 @@ function hexvalues(i){
 }
 
 function hover_pixel(pixel){
-    core_elements['color-hover'].value = pixel.textContent === 'T'
+    core_elements.color_hover.value = pixel.textContent === 'T'
       ? 'transparent'
       : rgb_to_hex(pixel.style.backgroundColor || 'rgb(0, 0, 0)');
 
-    const width = Math.floor(core_storage_data['width']);
-    core_elements['x'].textContent = core_digits_min({
+    const width = Math.floor(core_storage_data.width);
+    core_elements.x.textContent = core_digits_min({
       'number': width - pixel.id % width,
     });
-    core_elements['y'].textContent = core_digits_min({
-      'number': Math.floor(core_storage_data['height']) - Math.floor(pixel.id / width),
+    core_elements.y.textContent = core_digits_min({
+      'number': Math.floor(core_storage_data.height) - Math.floor(pixel.id / width),
     });
 
     if(core_pointer['down-0']){
@@ -56,9 +56,9 @@ function hover_pixel(pixel){
 function repo_init(){
     core_repo_init({
       'events': {
-        'file-to-uri': {
+        'file_to_uri': {
           'onclick': function(){
-              const files = core_elements['file'].files;
+              const files = core_elements.file.files;
               if(files.length === 0){
                   return;
               }
@@ -66,7 +66,7 @@ function repo_init(){
               core_file({
                 'file': files[0],
                 'todo': function(event){
-                    core_elements['uri'].value = event.target.result;
+                    core_elements.uri.value = event.target.result;
                 },
               });
           },
@@ -74,7 +74,7 @@ function repo_init(){
         'fill': {
           'onclick': fill,
         },
-        'grid-remake': {
+        'grid_remake': {
           'onclick': function(){
               if(!globalThis.confirm('Remake grid?')){
                   return;
@@ -84,10 +84,10 @@ function repo_init(){
               setup_dimensions();
           },
         },
-        'grid-toggle': {
+        'grid_toggle': {
           'onclick': grid_toggle,
         },
-        'uri-to-grid': {
+        'uri_to_grid': {
           'onclick': function(){
               if(!globalThis.confirm('Set grid to URI?')){
                   return;
@@ -102,11 +102,11 @@ function repo_init(){
         'edited': false,
         'pixelcount': 0,
       },
-      'info': '<textarea id=uri></textarea><br><canvas id=preview style="border:solid 10px #000"></canvas> <span id=uri-length></span><br>'
-        + '<input id=file type=file><button id=file-to-uri type=button>Convert File to URI</button><br>'
-        + '<button id=grid-remake type=button>Remake Grid</button><button id=grid-toggle type=button>Toggle Borders</button><button id=uri-to-grid type=button>Set Grid to URI</button><br>'
+      'info': '<textarea id=uri></textarea><br><canvas id=preview style="border:solid 10px #000"></canvas> <span id=uri_length></span><br>'
+        + '<input id=file type=file><button id=file_to_uri type=button>Convert File to URI</button><br>'
+        + '<button id=grid_remake type=button>Remake Grid</button><button id=grid_toggle type=button>Toggle Borders</button><button id=uri_to_grid type=button>Set Grid to URI</button><br>'
         + '<input id=color type=color value=#ffffff><button id=fill type=button>Fill</button>'
-        + ' <span id=x></span> <span id=y></span> <input class=mini id=color-hover readonly type=text>',
+        + ' <span id=x></span> <span id=y></span> <input class=mini id=color_hover readonly type=text>',
       'menu': true,
       'menu-block-events': false,
       'storage': {
@@ -127,19 +127,19 @@ function repo_init(){
       'title': 'PixelEditor.htm',
       'ui-elements': [
         'color',
-        'color-hover',
+        'color_hover',
         'edit',
         'file',
         'preview',
         'uri',
-        'uri-length',
+        'uri_length',
         'x',
         'y',
       ],
     });
 
     setup_dimensions();
-    core_elements['edit'].style.userSelect = 'none';
+    core_elements.edit.style.userSelect = 'none';
 }
 
 function rgb_to_hex(rgb){
@@ -155,8 +155,8 @@ function rgb_to_hex(rgb){
 }
 
 function setup_dimensions(){
-    const width = Math.floor(core_storage_data['width']);
-    pixelcount = Math.floor(core_storage_data['height']) * width;
+    const width = Math.floor(core_storage_data.width);
+    pixelcount = Math.floor(core_storage_data.height) * width;
 
     let loop_counter = pixelcount - 1;
     let output = '';
@@ -169,8 +169,8 @@ function setup_dimensions(){
         }
     }while(loop_counter--);
 
-    core_elements['edit'].innerHTML = output;
-    core_elements['edit'].style.minWidth = (width * core_storage_data['size']) + 'px';
+    core_elements.edit.innerHTML = output;
+    core_elements.edit.style.minWidth = (width * core_storage_data.size) + 'px';
 
     for(const element in core_elements){
         if(!globalThis.isNaN(element)){
@@ -184,9 +184,9 @@ function setup_dimensions(){
         style.backgroundColor = '#000';
         style.borderColor = '#aaa';
         style.borderWidth = '1px';
-        style.height = core_storage_data['size'] + 'px';
+        style.height = core_storage_data.size + 'px';
         style.margin = 0;
-        style.width = core_storage_data['size'] + 'px';
+        style.width = core_storage_data.size + 'px';
     }while(loop_counter--);
 
     core_elements[0].style.borderWidth = '1px';
@@ -200,18 +200,18 @@ function update_pixel(pixel, result){
       'mode',
     ]);
 
-    if(core_storage_data['mode'] === 1){
-        core_elements['color'].value = rgb_to_hex(pixel.style.backgroundColor);
+    if(core_storage_data.mode === 1){
+        core_elements.color.value = rgb_to_hex(pixel.style.backgroundColor);
 
-    }else if(core_storage_data['mode'] === 2){
+    }else if(core_storage_data.mode === 2){
         pixel.style.backgroundColor = '#000';
         pixel.textContent = 'T';
-        core_elements['color-hover'].value = 'transparent';
+        core_elements.color_hover.value = 'transparent';
 
     }else{
-        pixel.style.backgroundColor = core_elements['color'].value;
+        pixel.style.backgroundColor = core_elements.color.value;
         pixel.textContent = '';
-        core_elements['color-hover'].value = rgb_to_hex(pixel.style.backgroundColor);
+        core_elements.color_hover.value = rgb_to_hex(pixel.style.backgroundColor);
     }
 
     if(result === true){
@@ -222,9 +222,9 @@ function update_pixel(pixel, result){
 }
 
 function update_result(){
-    const height = Math.floor(core_storage_data['height']);
-    const width = Math.floor(core_storage_data['width']);
-    const canvas_element = core_elements['preview'];
+    const height = Math.floor(core_storage_data.height);
+    const width = Math.floor(core_storage_data.width);
+    const canvas_element = core_elements.preview;
     canvas_element.height = height;
     canvas_element.width = width;
 
@@ -263,26 +263,26 @@ function update_result(){
 
     const uri = core_uri({
       'element': canvas_element,
-      'quality': core_storage_data['quality'],
-      'type': core_storage_data['type'],
+      'quality': core_storage_data.quality,
+      'type': core_storage_data.type,
     });
-    core_elements['uri'].value = uri;
-    core_elements['uri-length'].innerHTML = uri.length;
+    core_elements.uri.value = uri;
+    core_elements.uri_length.innerHTML = uri.length;
 }
 
 function uri_to_grid(){
-    if(core_storage_data['uri'].length === 0){
+    if(core_storage_data.uri.length === 0){
         update_result();
         return;
     }
 
     core_image({
       'id': 'uri',
-      'src': core_storage_data['uri'],
+      'src': core_storage_data.uri,
       'todo': function(){
-          const canvas = core_elements['preview'].getContext('2d');
-          const height = Math.floor(core_storage_data['height']);
-          const width = Math.floor(core_storage_data['width']);
+          const canvas = core_elements.preview.getContext('2d');
+          const height = Math.floor(core_storage_data.height);
+          const width = Math.floor(core_storage_data.width);
 
           canvas.clearRect(
             0,
@@ -291,11 +291,11 @@ function uri_to_grid(){
             height
           );
           canvas.drawImage(
-            core_images['uri'],
+            core_images.uri,
             0,
             0
           );
-          delete core_images['uri'];
+          delete core_images.uri;
 
           let loop_counter = pixelcount - 1;
           let row_counter = height;
@@ -306,11 +306,11 @@ function uri_to_grid(){
                 1,
                 1
               );
-              if(pixel['data'][3] > 0){
+              if(pixel.data[3] > 0){
                   core_elements[loop_counter].style.backgroundColor = '#'
-                    + hexvalues((pixel['data'][0] - pixel['data'][0] % 16) / 16) + hexvalues(pixel['data'][0] % 16)
-                    + hexvalues((pixel['data'][1] - pixel['data'][1] % 16) / 16) + hexvalues(pixel['data'][1] % 16)
-                    + hexvalues((pixel['data'][2] - pixel['data'][2] % 16) / 16) + hexvalues(pixel['data'][2] % 16);
+                    + hexvalues((pixel.data[0] - pixel.data[0] % 16) / 16) + hexvalues(pixel.data[0] % 16)
+                    + hexvalues((pixel.data[1] - pixel.data[1] % 16) / 16) + hexvalues(pixel.data[1] % 16)
+                    + hexvalues((pixel.data[2] - pixel.data[2] % 16) / 16) + hexvalues(pixel.data[2] % 16);
                   core_elements[loop_counter].textContent = '';
 
               }else{
